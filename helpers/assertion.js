b0fe69135d59dcc30553de771f7417b4517bae5f
Charitha-SE <nadipannagaricharitha91@gmail.com>
2024-09-26 09:34:02 +0530

    Removed unnecessary files

--- helpers/assertion.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require('@playwright/test');
-
-async function assertResponseStatus(response, expectedStatus) {
-    expect(response.status()).toBe(expectedStatus);
-  }
-  
-  module.exports = { assertResponseStatus };
-  
